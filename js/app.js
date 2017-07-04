78e6e7cbf2748afdc3defe15291ec42c33d388a3
smitzter <smittybuckler@gmail.com>
2017-07-04 14:36:49 -0700

    first commit. psuedocode.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//////the base assignment/////////////
+//make constructor function
+//make products array
+
+//make for var i loop to go through array
+//make math random to choose random pics
+
+//render random image to page
+
+//refresh page after click
+
+//make stop after 25 clicks - event listener
+
+///local storage + clicks event listen////////
+
+/////////the chart///////////////////
+//make code to follow clicks, event listener
+//make code to display clicks in table
+
+///////animation/////
+///in html/css at least one CSS transition/animation of your choice.
