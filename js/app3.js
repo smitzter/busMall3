20afe21128dcfd8e4bd3f5e86fdfe85f5b7d5fdf
smitzter <smittybuckler@gmail.com>
2017-07-06 08:55:36 -0700

    removed comments for easier viewing

--- js/app3.js
+++ js/app3.js
@@ -1,23 +1,18 @@
 'use strict';
 
-// function Product(productName, path) {
-//   this.productName = productName;
-//   this.path = path;
-// }
-//array
 var products = ['babySweep', 'bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wineGlass'];
 console.log(products);
 var attempts = 0;
-var maxAttempts = 25;
+var maxAttempts = 5;
 var img1 = document.getElementById('img1');
 var img2 = document.getElementById('img2');
 var img3 = document.getElementById('img3');
-var productImagesParent = document.getElementById('imgParent');
+var productImagesParent = document.getElementById('productImagesParent');
 var responseElement = document.getElementById('response');
 
 setup();
 
-imgParent.addEventListener('click', clickedEventListener);
+productImagesParent.addEventListener('click', clickedEventListener);
 
 function setup() {
   img1 = generateRandomProduct();
@@ -32,10 +27,9 @@ function setup() {
   renderImg1(img1);
   renderImg2(img2);
   renderImg3(img3);
-  // updateAttempts();
+
   if (attempts === maxAttempts) {
-    // do things
-    imgParent.removeEventListener('click', clickedEventListener);
+    productImagesParent.removeEventListener('click', clickedEventListener);
   }
 }
 
@@ -64,7 +58,7 @@ function incrementAttempts() {
 }
 
 function updateAttemptsElement() {
-  imgParent.textContent = getAttempts() || 0;
+  productImagesParent.textContent = getAttempts() || 0;
 }
 
 function getAttempts() {
@@ -81,14 +75,10 @@ function createOrUpdateAttempts(value) {
   var attempts = localStorage.getItem('attemptsEl');
   return attempts;
 }
-// function updateAttempts() {
-//   attemptsEl.textContent = maxAttempts - attempts;
-// }
 
 function generateRandomProduct() {
   var index = Math.floor(Math.random() * products.length);
   return products[index];
-  //make math random to choose random pics
 }
 
 function renderImg1(img1) {
@@ -117,27 +107,3 @@ function renderImg3(img3) {
   img.setAttribute('height', '200px');
   productImagesParent.appendChild(img);
 }
-//make products array
-
-////====modify content====////
-//get placehoder id from html
-//render random image to page - appendChild
-
-//====program rules====//
-//refresh page after click
-
-//make stop after 25 clicks - event listener
-
-///local storage + clicks event listen////////
-//store local data
-//retrieve local data
-
-/////////==the chart==///////////////////
-//make code to follow clicks, event listener
-//make code to display clicks in table
-
-///////==animation==/////
-///in html/css at least one CSS transition/animation of your choice.
-
-//notes from Stephanie
-// what your functions expect as parameters, and what you're passing them. A method on an object knows about the object it's attached to, so you frequently don't need to pass it anything. Also, you reference productplaceImg, but I don't see it defined anywhere. And...what is pushImg? Looks like it's not used.
